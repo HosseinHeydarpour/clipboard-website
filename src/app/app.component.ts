8af668aa6ae828e4acd30b296d064ae6ca19199a
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-04-08 10:46:34 +0330

    feat: add feature component to the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './features/hero/hero.component';
 import { SnippetsComponent } from './features/snippets/snippets.component';
+import { FeaturesComponent } from './features/features/features.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeroComponent, SnippetsComponent],
+  imports: [HeroComponent, SnippetsComponent, FeaturesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
