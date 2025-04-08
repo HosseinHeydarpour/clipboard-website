f49242437045202942b929f5756c0740d820311b
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-04-08 10:55:26 +0330

    feat: add access anywhere section component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { HeroComponent } from './features/hero/hero.component';
 import { SnippetsComponent } from './features/snippets/snippets.component';
 import { FeaturesComponent } from './features/features/features.component';
+import { AnywhereClipboardAccessComponent } from './features/anywhere-clipboard-access/anywhere-clipboard-access.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeroComponent, SnippetsComponent, FeaturesComponent],
+  imports: [
+    HeroComponent,
+    SnippetsComponent,
+    FeaturesComponent,
+    AnywhereClipboardAccessComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/features/anywhere-clipboard-access/anywhere-clipboard-access.component.ts
+++ src/app/features/anywhere-clipboard-access/anywhere-clipboard-access.component.ts
@@ -5,8 +5,12 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './anywhere-clipboard-access.component.html',
-  styleUrl: './anywhere-clipboard-access.component.scss'
+  styleUrl: './anywhere-clipboard-access.component.scss',
 })
 export class AnywhereClipboardAccessComponent {
-
+  sectionData = {
+    title: 'Access Clipboard Anywhere',
+    description:
+      "Whether you're on the go, or at your computer, you can access all your Clipboard snippets in a few simple clicks.",
+  };
 }
