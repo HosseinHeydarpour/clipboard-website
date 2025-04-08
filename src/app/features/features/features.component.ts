d6593234d23febc1859858864bb5cb12d9eebdb0
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-04-08 10:47:16 +0330

    feat: add feature section dynamically

--- src/app/features/features/features.component.ts
+++ src/app/features/features/features.component.ts
@@ -1,12 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+interface Feature {
+  title: string;
+  description: string;
+}
 
 @Component({
   selector: 'app-features',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './features.component.html',
-  styleUrl: './features.component.scss'
+  styleUrl: './features.component.scss',
 })
 export class FeaturesComponent {
-
+  features: Feature[] = [
+    {
+      title: 'Quick Search',
+      description:
+        'Easily search your snippets by content, category, web address, application, and more.',
+    },
+    {
+      title: 'iCloud Sync',
+      description:
+        'Instantly saves and syncs snippets across all your devices.',
+    },
+    {
+      title: 'Colmpletely History',
+      description:
+        'Retrieve any snippets from the first moment you started using the app.',
+    },
+  ];
 }
