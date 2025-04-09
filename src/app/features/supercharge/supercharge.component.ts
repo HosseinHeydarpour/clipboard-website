5acf3a64f0488dae160b41e113769dbe178a4df2
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-04-09 09:50:46 +0330

    feat: render supercharge  section items dynamically

--- src/app/features/supercharge/supercharge.component.ts
+++ src/app/features/supercharge/supercharge.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
 
+interface Item {
+  title: string;
+  description: string;
+  image: string;
+}
+
 @Component({
   selector: 'app-supercharge',
   standalone: true,
   imports: [],
   templateUrl: './supercharge.component.html',
-  styleUrl: './supercharge.component.scss'
+  styleUrl: './supercharge.component.scss',
 })
 export class SuperchargeComponent {
-
+  items: Item[] = [
+    {
+      title: 'Create Blacklists',
+      description:
+        'Easily search your snippets by content, category, web address, application, and more.',
+      image: 'icon-blacklist.svg',
+    },
+    {
+      title: 'Plain Texxt Snippets',
+      description:
+        'Remove unwanted formatting from copied text for a consistent look.',
+      image: 'icon-text.svg',
+    },
+    {
+      title: 'Sneak Preview',
+      description:
+        'Quick preview of all snippets on your Clipboard for easy access.',
+      image: 'icon-preview.svg',
+    },
+  ];
 }
