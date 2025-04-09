e9af38baa2d3cf499f737394109e0d5bc73f1fe2
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-04-09 11:05:28 +0330

    feat: render footer menus dynamically

--- src/app/features/footer/footer.component.ts
+++ src/app/features/footer/footer.component.ts
@@ -1,12 +1,74 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+
+interface MenuItem {
+  label: string;
+  href: string;
+}
+
+interface Menu {
+  menu_name: string;
+  items: MenuItem[];
+}
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
+  menus: Menu[] = [
+    {
+      menu_name: 'Menu 1',
+      items: [
+        {
+          label: 'FAQs',
+          href: '',
+        },
+        {
+          label: 'Contact Us',
+          href: '',
+        },
+      ],
+    },
+    {
+      menu_name: 'Menu 2',
+      items: [
+        {
+          label: 'Privacy Policy',
+          href: '',
+        },
+        {
+          label: 'Press Kit',
+          href: '',
+        },
+      ],
+    },
+    {
+      menu_name: 'Menu 3',
+      items: [
+        {
+          label: 'Install Guide',
+          href: '',
+        },
+      ],
+    },
+  ];
 
+  socialLogos = [
+    {
+      src: 'icon-facebook.svg',
+      alt: 'Facebook logo',
+    },
+    {
+      src: 'icon-instagram.svg',
+      alt: 'Instagram logo',
+    },
+    {
+      src: 'icon-twitter.svg',
+      alt: 'Twitter logo',
+    },
+  ];
 }
