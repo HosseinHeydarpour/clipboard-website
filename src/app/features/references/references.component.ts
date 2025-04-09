0acdf4079a9fa3d37b3e320643ae0d069b5e5b1d
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-04-09 10:07:51 +0330

    feat: render reference logos dynamically from JSON

--- src/app/features/references/references.component.ts
+++ src/app/features/references/references.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
 
+interface Logo {
+  src: string;
+  alt: string;
+}
+
 @Component({
   selector: 'app-references',
   standalone: true,
   imports: [],
   templateUrl: './references.component.html',
-  styleUrl: './references.component.scss'
+  styleUrl: './references.component.scss',
 })
 export class ReferencesComponent {
-
+  logos: Logo[] = [
+    {
+      src: 'logo-google.png',
+      alt: '',
+    },
+    {
+      src: 'logo-ibm.png',
+      alt: '',
+    },
+    {
+      src: 'logo-microsoft.png',
+      alt: '',
+    },
+    {
+      src: 'logo-hp.png',
+      alt: '',
+    },
+    {
+      src: 'logo-vector-graphics.png',
+      alt: '',
+    },
+  ];
 }
