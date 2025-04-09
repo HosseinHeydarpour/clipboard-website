e5db403741cacf6c933ac84d43bd3de55ae97e53
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-04-09 09:31:02 +0330

    feat: add supercharge section component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { HeroComponent } from './features/hero/hero.component';
 import { SnippetsComponent } from './features/snippets/snippets.component';
 import { FeaturesComponent } from './features/features/features.component';
 import { AnywhereClipboardAccessComponent } from './features/anywhere-clipboard-access/anywhere-clipboard-access.component';
+import { SuperchargeComponent } from './features/supercharge/supercharge.component';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +13,7 @@ import { AnywhereClipboardAccessComponent } from './features/anywhere-clipboard-
     SnippetsComponent,
     FeaturesComponent,
     AnywhereClipboardAccessComponent,
+    SuperchargeComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
---
+++ src/app/features/supercharge/supercharge.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-supercharge',
+  standalone: true,
+  imports: [],
+  templateUrl: './supercharge.component.html',
+  styleUrl: './supercharge.component.scss'
+})
+export class SuperchargeComponent {
+
+}
